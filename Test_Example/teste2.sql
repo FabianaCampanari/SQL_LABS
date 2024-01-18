416c2162d8ae42a03445b9162fce3fc00404ebf6
Fabiana Campanari <fabicampanari@gmail.com>
2024-01-18 07:34:33 -0300

    Delete Test_Example/teste2.sql
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Test_Example/teste2.sql
+++
@@ -1,4 +0,0 @@
--- END: Test if table Produto is created successfully
--- BEGIN: Test if table Produto is created successfully
-SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Produto';
--- END: Test if table Produto is created successfully
