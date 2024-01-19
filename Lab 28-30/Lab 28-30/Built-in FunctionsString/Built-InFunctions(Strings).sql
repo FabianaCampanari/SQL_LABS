da7b2a727b724676d910ffee84d8092ab5940d5e
Fabiana Campanari <fabicampanari@gmail.com>
2024-01-19 08:00:36 -0300

    Add files via upload
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

---
+++ Lab 28-30/Lab 28-30/Built-in FunctionsString/Built-InFunctions(Strings).sql
@@ -0,0 +1,32 @@
+/*
+
+	NomeFunção([Argumentos])
+
+	LEN(Texto Pesquisado)
+
+	LEFT(Texto Pesquisado,Nº de caracteres)
+
+	RIGHT(Texto Pesquisado,Nº de caracteres)
+
+	REPLACE(Texto Pesquisado,Texto Localizado,Texto Substituição)
+
+	CHARINDEX(Texto Pesquisado,Texto Localizado,Posição Inicial)
+
+	SUBSTRING(Texto Pesquisado,Posição Inicial,Nº de Caracteres)
+
+	RTRIM(Texto Pesquisado)
+
+	LTRIM(Texto Pesquisado)
+
+	UPPER(Texto Pesquisado)
+
+	LOWER(Texto Pesquisado)
+
+	REPLICATE(Texto a Repetir,Quantidade de Vezes)
+
+	REVERSE(Texto a ser Invertido)
+	
+	STRING_SPLIT(Texto a ser separado,Caractere delimitador)
+
+*/
+
