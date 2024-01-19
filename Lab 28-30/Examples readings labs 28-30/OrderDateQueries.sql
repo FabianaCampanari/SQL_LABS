c9a6ba3f5efcb4247c554d62bd29b8caba358b74
Fabiana Campanari <fabicampanari@gmail.com>
2024-01-19 08:17:57 -0300

    Update OrderDateQueries.sql
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 28-30/Examples readings labs 28-30/OrderDateQueries.sql
+++ Lab 28-30/Examples readings labs 28-30/OrderDateQueries.sql
@@ -32,4 +32,6 @@ WHERE DATA_EMISSAO BETWEEN '2014.1.1' AND '2014.1.31'
 /* 5. Listar todos os pedidos do Vendedor
       C�digo 1 em Jan/2014. */
 SELECT * FROM TB_PEDIDO
+WHERE DATA_EMISSAO BETWEEN '2014.1.1' AND '2014.1.31'
+      AND CODVEN = 1;
 
