dca9b56587a54f84f89442406599531f090a401b
Fabiana Campanari <fabicampanari@gmail.com>
2024-01-20 14:42:51 -0300

    Update ProductInventoryQueries.sql
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 28-30/Examples readings labs 28-30/ProductInventoryQueries.sql
+++ Lab 28-30/Examples readings labs 28-30/ProductInventoryQueries.sql
@@ -164,3 +164,9 @@ ORDER BY SALARIO DESC;
 -- aqueles empatados com o �ltimo
 SELECT TOP 7 WITH TIES CODFUN, NOME, SALARIO FROM TB_EMPREGADO 
 ORDER BY SALARIO DESC;
+
+
+-- Listar os 7 empregados que ganham mais, inclusive 
+-- aqueles empatados com o �ltimo
+SELECT TOP 7 WITH TIES CODFUN, NOME, SALARIO FROM TB_EMPREGADO 
+ORDER BY SALARIO DESC;
