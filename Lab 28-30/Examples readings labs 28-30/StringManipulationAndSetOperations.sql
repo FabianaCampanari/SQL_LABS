dff1f9ef38c4b7de59b390e5e34b4e1113e759ee
Fabiana Campanari <fabicampanari@gmail.com>
2024-01-28 20:48:49 -0300

    Update StringManipulationAndSetOperations.sql
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 28-30/Examples readings labs 28-30/StringManipulationAndSetOperations.sql
+++ Lab 28-30/Examples readings labs 28-30/StringManipulationAndSetOperations.sql
@@ -39,4 +39,4 @@ SELECT NOME, SALARIO, DATA_ADMISSAO,
            WHEN 1 THEN 'Domingo'
            WHEN 2 THEN 'Segunda-Feira'
            WHEN 3 THEN 'Ter�a-Feira'
-           WHEN 4 
+           WHEN 4 THEN 'Quarta-Feira'
