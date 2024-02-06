b7fec6eac0426812992e39bcabd42378024a6c32
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-05 22:12:52 -0300

    Test
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- media.sql
+++ media.sql
@@ -1,6 +1,5 @@
 
 -- teste
-
 select p.nome, v.Data, v.ValorTotal, v.Desconto, vd.Quantidade, ((v.ValorTotal-
 v.desconto) * vd.Quantidade) vl_unitario from Produto p left join VendaDetalhe vd
 on vd.ProdutoId = p.Id left join Vendas v on vd.VendasId = v.VendaId
