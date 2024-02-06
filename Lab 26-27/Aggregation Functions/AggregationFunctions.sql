23af81de1e0640bd34a033cfb6aa02752c3b39c4
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-05 21:50:16 -0300

    Lab 26-27
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 26-27/Aggregation Functions/AggregationFunctions.sql
+++ Lab 26-27/Aggregation Functions/AggregationFunctions.sql
@@ -1,24 +1,24 @@
+
 USE SisDep;
+-- Retornar o total geral de sal√°rios pagos
+SELECT SUM(salario) AS [Total de Sal√°rios] FROM Funcionario;
 
--- Retornar o total geral de sal·rios pagos
-SELECT SUM(salario) AS [Total de Sal·rios] FROM Funcionario;
+-- Retornar a m√©dia de sal√°rios
+SELECT AVG(salario) AS [M√©dia Salarial] FROM Funcionario;
 
--- Retornar a mÈdia de sal·rios
-SELECT AVG(salario) AS [MÈdia Salarial] FROM Funcionario;
-
--- Mais de uma agregaÁ„o no mesmo comando SELECT
+-- Mais de uma agrega√ß√£o no mesmo comando SELECT
 
 SELECT
-	MAX(SALARIO) AS [Maior Sal·rio],
-	MIN(SALARIO) AS [Menor Sal·rio],
-	COUNT(SALARIO) AS [N∫ de Funcion·rios]
+	MAX(SALARIO) AS [Maior Sal√°rio],
+	MIN(SALARIO) AS [Menor Sal√°rio],
+	COUNT(SALARIO) AS [N¬∫ de Funcion√°rios]
 FROM Funcionario;
 
 USE SysConVendas;
 SELECT * FROM Dados;
 
 SELECT COUNT(Vendedor) AS Contagem_Coluna FROM Dados;
-
+--
 SELECT COUNT(pedido) AS Contagem_Coluna FROM Dados;
-
+--
 SELECT COUNT(*) AS Contagem_Linha FROM Dados;
