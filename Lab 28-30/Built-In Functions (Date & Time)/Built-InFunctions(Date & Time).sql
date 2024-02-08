e1c17dc79c06724ff22e680e711338b1570cd018
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-07 21:54:06 -0300

    Lab 28-30
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

---
+++ Lab 28-30/Built-In Functions (Date & Time)/Built-InFunctions(Date & Time).sql
@@ -0,0 +1,32 @@
+/*
+
+	NomeFunção([Argumentos])
+
+	GETDATE()
+
+	DAY(Data Referência)
+
+	MONTH(Data Referência)
+
+	YEAR(Data Referência)
+
+	EMONTH(Data Referência,Quantidade de meses a adicionar)
+
+	DATEFROMPARTS(Ano,Mês,Dia)
+
+	DATEDIFF(Parte Data,Data Inicial,Data Final)
+
+	DATEADD(Parte Data,valor a ser adicionado,Data Referência)
+
+	DATENAME(Data Referência,Parte Data)
+
+	onde Parte Data poderá ser:
+
+		> YEAR		YYYY
+		> QUARTER	Q
+		> MONTH		M
+		> WEEK		WW
+		> DAY		D
+
+*/
+
