93b5414db19da45f4a10ab292705afff3fe85091
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-14 01:15:12 -0300

    Lab 28-30
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

---
+++ Lab 28-30/Built-In Functions (Conversion and Format)/Built-InFunctions(Conversion and Format).sql
@@ -0,0 +1,25 @@
+/*
+
+	NomeFunção([Argumentos])
+
+	CAST(Valor Referência AS Tipo de Dado)
+
+	CONVERT(Tipo de Dado,Valor Referência,Estilo)
+
+	Onde Estilo será aplicado para datas e possui códigos de 1 a 14 (Ano Sem Século)
+
+	e 101 1 114 (Ano Com Século)
+
+	FORMAT(Valor Referência,Formato,Cultura/País)
+
+	EX:
+
+	Format(Campo Monetário,'C','en-us')
+	Format(Campo Monetário,'C','pt-br')
+
+	Format(Campo Data,'d','pt-br')
+	Format(Campo Data,'D','pt-br')
+	Format(Campo Data,'dd - mm - yyyy','pt-br')
+
+*/
+
