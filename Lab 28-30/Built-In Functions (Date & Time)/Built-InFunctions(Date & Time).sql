eb33e2e5db3b2968460a67897e6e9a677a28e816
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-14 01:21:44 -0300

    Lab 29-30
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 28-30/Built-In Functions (Date & Time)/Built-InFunctions(Date & Time).sql
+++ Lab 28-30/Built-In Functions (Date & Time)/Built-InFunctions(Date & Time).sql
@@ -12,4 +12,13 @@
 
 	EMONTH(Data Refer�ncia,Quantidade de meses a adicionar)
 
+        DATEFROMPARTS(Ano,M�s,Dia)
+
+	DATEDIFF(Parte Data,Data Inicial,Data Final)
+
+	DATEADD(Parte Data,valor a ser adicionado,Data Refer�ncia)
+
+	DATENAME(Data Refer�ncia,Parte Data)
+
+	
 
