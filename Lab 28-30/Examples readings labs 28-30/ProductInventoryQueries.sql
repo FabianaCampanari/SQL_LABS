d091b6b2f509ff937bd32ab6fd0051e979465a62
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-17 16:57:40 -0300

    Update ProductInventoryQueries.sql
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 28-30/Examples readings labs 28-30/ProductInventoryQueries.sql
+++ Lab 28-30/Examples readings labs 28-30/ProductInventoryQueries.sql
@@ -169,10 +169,7 @@ WHERE SALARIO > 5000
 ORDER BY SALARIO;
 
 --Mostrando os funcion�rios com campo COD_DEPTO menor ou igual a 3
-
---Mostrando os funcion�rios com campo COD_DEPTO menor ou igual a 3
 SELECT * FROM TB_EMPREGADO 
 WHERE COD_DEPTO <= 3
 ORDER BY COD_DEPTO;
 
---Mostrando os funcion�rios com campo COD_DEPTO igual a 2
