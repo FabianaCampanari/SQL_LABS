09f2834507f1cad14fa7e6a5bbbbeff7487c223e
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-23 22:44:16 -0300

    Update StringAndDateManipulationQueries.sql
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

--- Lab 28-30/Examples readings labs 28-30/StringAndDateManipulationQueries.sql
+++ Lab 28-30/Examples readings labs 28-30/StringAndDateManipulationQueries.sql
@@ -5,10 +5,10 @@ USE PEDIDOS
 SELECT NOME, SALARIO, DATA_ADMISSAO, 
        CASE DATEPART(MONTH,DATA_ADMISSAO)
        WHEN 1 THEN 'Janeiro'
-	    WHEN 2 THEN 'Fevereiro'
-	    WHEN 3 THEN 'Mar�o'
-	    WHEN 4 THEN 'Abril'
-	    WHEN 5 THEN 'Maio'
+       WHEN 2 THEN 'Fevereiro'
+       WHEN 3 THEN 'Mar�o'
+       WHEN 4 THEN 'Abril'
+       WHEN 5 THEN 'Maio'
 	    WHEN 6 THEN 'Junho'
 	    WHEN 7 THEN 'Julho'      
 	    WHEN 8 THEN 'Agosto'      
